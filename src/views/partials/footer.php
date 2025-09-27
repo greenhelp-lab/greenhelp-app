7f35306580558f4fbcfcfa55969f4c2fdf60bb98
RafaelReis29 <rafael.dnreis@gmail.com>
2025-09-27 14:48:41 -0300

    :sparkles: feat(footer): added footer's html basic structure

---
+++ src/views/partials/footer.php
@@ -0,0 +1,13 @@
+<footer>
+<ul>
+  <li><a href="">Sobre Nós</a></li>
+  <li><a href="">Contato</a></li>
+  <li><a href="">Política de Privacidade</a></li>
+  <li><a href="">Termos de Serviço</a></li>
+  <li><a href="">Conheça a Equipe</a></li>
+</ul>
+<div class="footer-row">
+   <img src="" alt="">
+    <p>&copy; 2025 GreenHelp. Todos os direitos reservados.</p>
+</div>
+</footer>
