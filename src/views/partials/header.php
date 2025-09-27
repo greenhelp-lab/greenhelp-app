e13b6fa2365bff1c757a8121c36d40d65cf2c1b4
RafaelReis29 <rafael.dnreis@gmail.com>
2025-09-27 14:38:18 -0300

    :sparkles: feat(header): added header's html basic structure

---
+++ src/views/partials/header.php
@@ -0,0 +1,37 @@
+<header class="header">
+    <div class="header-container">
+      <!-- Logo da GreenHelp -->
+      <div class="logo">
+        <img src="" alt="Logo da GreenHelp">
+      </div>
+
+      <!-- Botão de notificações -->
+      <button class="btn-notifications" aria-label="Notificações">
+        
+      </button>
+
+      <!-- Links / Ícones para navegação na plataforma -->
+      <nav class="nav">
+        <ul class="nav-list">
+          <li class="nav-item">
+            <a href="" class="nav-link">
+              <img src="" alt="ícone de home">
+              <span class="text">Home</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">
+              <img src="" alt="ícone de serviços">
+              <span class="text">Serviços</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">
+              <img src="" alt="ícone de conta">
+              <span class="text">Conta</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
