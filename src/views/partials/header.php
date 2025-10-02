dbf7e51ee7c7d517aac648fe1d6b277168bbc1da
RafaelReis29 <rafael.dnreis@gmail.com>
2025-10-01 21:23:28 -0300

    🔧 update(/partials): updated paths for imgs on 'header.php' & 'footer.php'; achor that goes to the home page temporarily excluded from header

--- src/views/partials/header.php
+++ src/views/partials/header.php
@@ -2,12 +2,12 @@
     <div class="header-container">
       <!-- Logo da GreenHelp -->
       <div class="logo">
-        <img src="" alt="Logo da GreenHelp">
+        <img src="/src/views/assets/imgs/logo.png" alt="Logo da GreenHelp">
       </div>
 
       <!-- Botão de notificações -->
       <button class="btn-notifications" aria-label="Notificações">
-        
+        <img src="/src/views/assets/imgs/bell-icon.svg" alt="ícone de sino de notificações">
       </button>
 
       <!-- Links / Ícones para navegação na plataforma -->
@@ -15,19 +15,13 @@
         <ul class="nav-list">
           <li class="nav-item">
             <a href="" class="nav-link">
-              <img src="" alt="ícone de home">
-              <span class="text">Home</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="" class="nav-link">
-              <img src="" alt="ícone de serviços">
+              <img src="/src/views/assets/imgs/services-area-icon.svg" alt="ícone de área de serviços">
               <span class="text">Serviços</span>
             </a>
           </li>
           <li class="nav-item">
             <a href="" class="nav-link">
-              <img src="" alt="ícone de conta">
+              <img src="/src/views/assets/imgs/account-icon.svg" alt="ícone de conta">
               <span class="text">Conta</span>
             </a>
           </li>
