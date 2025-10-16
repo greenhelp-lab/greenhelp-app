53a4b6aba84b9d69755ca925a5cbd3e3ab73c73c
RafaelReis29 <rafael.dnreis@gmail.com>
2025-10-16 18:21:22 -0300

    :wrench: nova estrutura de pastas, arquivo confg.php iniciado e alterações no header, todos para concertar as URLS com o php

---
+++ config.php
@@ -0,0 +1,3 @@
+<?php
+define('BASE_PATH', $_SERVER['DOCUMENT_ROOT'] . '/greenhelp-app'); // Define o caminho base da plataforma - bom para usar em qualquer caminho de PHP!
+define('BASE_URL', '/greenhelp-app'); // Define a URL base da plataforma - bom para usar em links de CSS, JS, imagens, etc.
--- src/pages/home/home_cliente.php
+++ src/pages/home/home_cliente.php
@@ -1,20 +1,22 @@
+<?php include_once '../../../config.php'; ?> <!-- Inclui o arquivo de configuração para usar BASE_PATH corretamente-->
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>GreenHelp Home</title>
-  <link rel="stylesheet" href="../../../assets/global.css">
-  <link rel="stylesheet" href="/greenhelp-app/src/pages/home/cliente/home_cliente.css">
+  <link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/css/global.css">
+  <link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/css//home/home_cliente.css">
 </head>
+
 <body>
-  <!-- <?php phpinfo(); ?> -->
-  <?php include __DIR__ . '../../../partials/header/header.php'; ?> <!-- 'include' header php for code optimization -->
-   <main class="container">
+  <?php include BASE_PATH . "/src/pages/partials/header.php"; ?> <!-- 'include' no header para otimizar código -->
+  <main class="container">
     <h1>Home</h1>
 
     <div class="user-photo">
-      <img src="../imgs/foto_user.png" alt="Alterar foto do usuário">
+      <img src="<?php echo BASE_URL; ?>/public/imgs/foto_user.png" alt="Alterar foto do usuário">
     </div>
 
     <h1>Nome da empresa</h1>
@@ -28,17 +30,17 @@
       <input type="password" placeholder="Senha">
       <input type="text" placeholder="ID da Empresa">
 
-    <section class="pontuacoes">
+      <section class="pontuacoes">
         <div class="pontuacoes-header">
           <img src="../imgs/pontuação_verde.png" alt="Pontuações Verdes" class="pontuacoes-img">
           <h2 class="pontuacoes-title">Pontuações Verdes</h2>
         </div>
-      
+
         <p class="pontuacoes-desc">
-          Ganhe mais pontos através da <span class="cor_verde">compra de serviços</span> e 
+          Ganhe mais pontos através da <span class="cor_verde">compra de serviços</span> e
           <span class="cor_verde">melhoras sustentáveis</span> na sua empresa
         </p>
-      
+
         <div class="niveis">
           <div class="nivel-card">
             <div class="nivel-left">
@@ -50,7 +52,7 @@
               <div class="progress" style="width: 60%;"></div>
             </div>
           </div>
-      
+
           <div class="nivel-card">
             <div class="nivel-left">
               <span class="nivel">Nível 7</span>
@@ -61,7 +63,7 @@
               <div class="progress" style="width: 60%;"></div>
             </div>
           </div>
-      
+
           <div class="nivel-card">
             <div class="nivel-left">
               <span class="nivel">Nível 7</span>
@@ -72,7 +74,7 @@
               <div class="progress" style="width: 60%;"></div>
             </div>
           </div>
-      
+
           <div class="nivel-card">
             <div class="nivel-left">
               <span class="nivel">Nível 7</span>
@@ -83,7 +85,7 @@
               <div class="progress" style="width: 60%;"></div>
             </div>
           </div>
-      
+
           <div class="nivel-card">
             <div class="nivel-left">
               <span class="nivel">Nível 7</span>
@@ -95,11 +97,12 @@
             </div>
           </div>
         </div>
-      
+
         <p class="pontuacao-total">Pontuação Total: <strong>4769</strong></p>
       </section>
 
-  </section>
-  <?php include __DIR__ . '../../../partials/footer/footer.php'; ?> <!-- 'include' footer php for code optimization -->
+      </section>
+      <?php include BASE_PATH . "/src/pages/partials/footer.php"; ?> <!-- 'include' no footer para otimizar código -->
 </body>
+
 </html>
