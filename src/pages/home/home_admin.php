e744d511debf818e1cf2bcad723339c6812b0eab
RafaelReis29 <rafael.dnreis@gmail.com>
2025-10-17 12:07:27 -0300

    ajuste nos links de include_once das páginas home

--- src/pages/home/home_admin.php
+++ src/pages/home/home_admin.php
@@ -1,4 +1,4 @@
-<?php include_once BASE_PATH . '/config.php'; ?> <!-- Inclui o arquivo de configuração para usar BASE_PATH corretamente-->
+<?php include_once $_SERVER['DOCUMENT_ROOT'] . '/greenhelp-app/config.php'; ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
