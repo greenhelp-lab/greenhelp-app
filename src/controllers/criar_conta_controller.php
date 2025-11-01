ae9978a29fec8da402bbc8725bc7c8ced66300e9
RafaelReis29 <rafael.dnreis@gmail.com>
2025-11-01 14:10:55 -0300

    mudanças de links, placeholders e encriptação

--- src/controllers/criar_conta_controller.php
+++ src/controllers/criar_conta_controller.php
@@ -1,11 +1,13 @@
 <?php
 session_start();
+require_once $_SERVER['DOCUMENT_ROOT'] . '/src/config/conexao.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/src/config/config.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   // validação dos dados do usuário
   $nome = $_POST['nome'];
   $email = $_POST['email'];
-  $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
+  $senha = password_hash($_POST['senha'], PASSWORD_BCRYPT);
 
   // pega os dados da empresa da sessão
   if (isset($_SESSION['empresa'])) {
@@ -13,18 +15,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // exemplo de inserção usando PDO
     $stmt = $pdo->prepare("
-            INSERT INTO empresas (nome_empresa, cnpj, endereco)
-            VALUES (:nome_empresa, :cnpj, :endereco)
+            INSERT INTO empresas (nome_empresa, cnpj, setor, tamanho_empresa)
+            VALUES (:nome_empresa, :cnpj, :setor, :tamanho_empresa)
         ");
     $stmt->execute([
       ':nome_empresa' => $empresa['nome_empresa'],
       ':cnpj' => $empresa['cnpj'],
-      ':endereco' => $empresa['endereco']
+      ':setor' => $empresa['setor'],
+      ':tamanho_empresa' => $empresa['tamanho_empresa']
     ]);
 
     $empresa_id = $pdo->lastInsertId();
 
-    // agora insere o usuário
     $stmt = $pdo->prepare("
             INSERT INTO usuarios (empresa_id, nome, email, senha)
             VALUES (:empresa_id, :nome, :email, :senha)
@@ -39,12 +41,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // limpa sessão
     unset($_SESSION['empresa']);
 
-    // redireciona ou mostra sucesso
-    header('Location: cadastro_sucesso.php');
+    // redireciona para página home
+    header('Location: ' . BASE_URL . '/src/pages/home/home_cliente.php');
     exit;
   } else {
-    // se não houver dados da empresa na sessão
-    header('Location: criar_empresa.php');
+    // se não houver dados da empresa na sessão, volta para a página de criar empresa para que o usuário forneça os dados
+    header('Location: ' . BASE_URL . '/src/pages/login/criar_empresa.php');
     exit;
   }
 }
