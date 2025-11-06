a1a49e8c60f7557fcf9dd82bfde5f5da96989b50
RafaelReis29 <rafael.dnreis@gmail.com>
2025-11-06 13:48:00 -0300

    🧪 test(logout): redirecionamento usando config.php e BASE_URL

--- src/controllers/logout.php
+++ src/controllers/logout.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
+require_once $_SERVER['DOCUMENT_ROOT'] . '/src/config/config.php';
 session_unset();  // limpa todas as variáveis da sessão
 session_destroy(); // encerra a sessão
-header("Location: landing.php"); // redireciona para a página inicial
+header("Location: ' . BASE_URL . '/public/sobre_nos.php"); // redireciona para a página sobre_nós, famosa landing page!
 exit();
