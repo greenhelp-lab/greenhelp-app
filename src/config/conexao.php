96c2dbfaf2ae8dedc94aa5d30d721345f3cebf02
RafaelReis29 <rafael.dnreis@gmail.com>
2025-11-15 15:42:26 -0300

    nova conexão com o banco cloud da Aiven e alteração dos nomes da landing page

--- src/config/conexao.php
+++ src/config/conexao.php
@@ -6,12 +6,19 @@ if (basename(__FILE__) == basename($_SERVER['PHP_SELF'])) {
   exit('Acesso direto negado.');
 }
 
+$env = parse_ini_file($_SERVER['DOCUMENT_ROOT'] . '/greenhelp-app/.env');
+$dbhost = $env["DB_HOST"];
+$dbuser = $env["DB_USER"];
+$dbpass = $env["DB_PASS"];
+$dbname = $env["DB_NAME"];
+$dbport = $env["PORT"];
+
 try {
   // cria objeto PDO e define modo de erro para exceções
   $pdo = new PDO(
-    "mysql:host=" . $_ENV['DB_HOST'] . ";port=" . $_ENV['DB_PORT'] . ";dbname=" . $_ENV['DB_NAME'],
-    $_ENV['DB_USER'],
-    $_ENV['DB_PASSWORD'],
+    "mysql:host=" . $dbhost . ";port=" . $dbport . ";dbname=" . $dbname,
+    $dbuser,
+    $dbpass,
     [
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
       PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
--- src/controllers/logout.php
+++ src/controllers/logout.php
@@ -2,5 +2,5 @@
 session_start();
 session_unset();  // limpa todas as variáveis da sessão
 session_destroy(); // encerra a sessão
-header("Location: /greenhelp-app/public/sobre_nos.html"); // redireciona para a página sobre_nós, famosa landing page!
+header("Location: /greenhelp-app/public/index.html"); // redireciona para a página sobre_nós, famosa landing page!
 exit();
