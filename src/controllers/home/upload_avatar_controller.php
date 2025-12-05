642e3a54b56d4cb494eae70c244235cb3a27045a
RafaelReis29 <rafael.dnreis@gmail.com>
2025-12-05 17:50:31 -0300

    melhorias adicionais

--- src/controllers/home/upload_avatar_controller.php
+++ src/controllers/home/upload_avatar_controller.php
@@ -1,9 +1,4 @@
 <?php
-declare(strict_types=1);
-
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
 session_start();
 
 require_once $_SERVER['DOCUMENT_ROOT'].'/greenhelp-app/src/config/config.php';
